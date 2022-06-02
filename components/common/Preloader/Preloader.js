00b62afa0e4043309606f5bc647b9127fad28d42
Esenushka <prosterling.mien@gmail.com>
2022-06-02 20:53:19 +0600

    fix

--- components/common/Preloader/Preloader.js
+++ components/common/Preloader/Preloader.js
@@ -3,18 +3,15 @@ import Image from "next/image"
 
 const Preloader = ({ full }) => {
     return (
-        <div className={css.wrapper}>
+        <div className={full ? css.wrapper : css.pad}>
             <div className={css.container}>
                 <svg className={css.loader} viewBox="0 0 340 340">
                     <circle className={css.circle} cx="170" cy="170" r="160" stroke="#66097b" />
                     <circle className={css.circle} cx="170" cy="170" r="135" stroke="#363A40" />
                     <circle className={css.circle} cx="170" cy="170" r="110" stroke="#66097b" />
                     <circle className={css.circle} cx="170" cy="170" r="85" stroke="#363A40" />
-                    {/*<circle cx="170" cy="170" r="60" stroke="#00bdd0"/>*/}
                 </svg>
-                <div className={css.img}>
-                <Image width={80} height={80} src="/b2.png"  alt="Bilal" />
-                </div>
+                <Image width={80} height={80} src="/b2.png" className={css.img} alt="Bilal" />
             </div>
         </div>
     );
